8826ea69e1eeaf529a99c10a54dc349c729def25
Ashish <parabashish92@gmail.com>
2026-06-06 12:26:10 +0530

    Updated backend URL

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -16,7 +16,7 @@ function App() {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/upload",
+        "https://smart-study-planner-7iz9.onrender.com",
         formData
       );
 
